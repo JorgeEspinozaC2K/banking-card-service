8f7657d8d469582be525363df5c804b42061e5ef
Jorge Andres Espinoza Canales <jorge.espinoza.c22@gmail.com>
2022-04-15 00:41:40 -0500

    Initial commit 15-04-22

---
+++ src/main/java/com/banking/card/service/app/BankingCardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.banking.card.service.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingCardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingCardServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banking/card/service/app/BankingCardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.card.service.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingCardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
