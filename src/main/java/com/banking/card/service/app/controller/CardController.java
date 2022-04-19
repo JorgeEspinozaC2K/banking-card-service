a4fecc74b438fdbadeec9e5ae2e4ad20a1ce2674
Jose Michael Condor Carhuapoma <cmikaelc19@gmail.com>
2022-04-19 14:09:56 -0500

    upload CardController

--- src/main/java/com/banking/card/service/app/controller/CardController.java
+++ src/main/java/com/banking/card/service/app/controller/CardController.java
@@ -2,9 +2,36 @@ package com.banking.card.service.app.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.banking.card.service.app.model.Card;
+import com.banking.card.service.app.service.CardService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+@RestController
+@RequestMapping("/api/v1/card")
 public class CardController {
 
 	private static final Logger log = LoggerFactory.getLogger(CardController.class);
 	
+	@Autowired
+	private CardService cardService;
+	
+	//This method displays all the information from the Card database
+	@GetMapping
+	public Flux<Card> index(){
+		return cardService.findAll();
+	}
+	@PostMapping("/save")
+	public Mono<Card> save(@RequestBody Card card) {
+		return cardService.save(card);
+	}
 }
